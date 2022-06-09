6828083db22932cc4f890e59654257b6bb678d8c
Alex_64 <go.it.6464@gmail.com>
2022-06-09 16:53:15 +0300

    Закончил add() c ОДНОСВЯЗНОЙ очередью !!!!!

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,21 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] organs) throws IOException {
+//        Queue
         MyQueue sama_myQueue = new MyQueue();
         sama_myQueue.add("ONE !");
         sama_myQueue.add("TWO !!");
         sama_myQueue.add("THREE !!!");
-//        sama_myQueue.add("FOUR !!!!");
-//        sama_myQueue.add("FIVE !!!!!");
+        sama_myQueue.add("FOUR !!!!");
+        sama_myQueue.add("FIVE !!!!!");
         System.out.println("main(). На данный момент у нас есть " + sama_myQueue.size + " нод");
         System.out.println("main(). sama_myQueue = " + sama_myQueue);
 
         System.out.println("sama_myQueue.peek() = " + sama_myQueue.peek());
-//        System.out.println("Удаляем елемент по индексу 1");
-//        sama_myQueue.remove(3);
+        System.out.println("Удаляем елемент по индексу 2");
+        sama_myQueue.remove(2);
+        System.out.println("main(). sama_myQueue = " + sama_myQueue);
+        sama_myQueue.clear();
 
 
     }
--- src/MyQueue.java
+++ src/MyQueue.java
@@ -3,107 +3,46 @@ import javax.lang.model.element.Element;
 public class MyQueue {
 //--------------------
     public Node firstNode;
-//    private Node firstNode;
-//
-//    public Node getFirstNode() {
-//        return firstNode;
-//    }
-//
-//    public void setFirstNode(Node firstNode) {
-//        this.firstNode = firstNode;
-//    }
 //--------------------
     public Node lastNode;
-//    private Node lastNode;
-//
-//    public Node getLastNode() {
-//        return lastNode;
-//    }
-//
-//    public void setLastNode(Node lastNode) {
-//        this.lastNode = lastNode;
-//    }
 //--------------------
     public int size = 0;
-//    private int size = 0;
-//
-//    public int getSize() {
-//        return size;
-//    }
-//
-//    public void setSize(int size) {
-//        this.size = size;
-//    }
-
     public MyQueue() {
         System.out.println("КОНСТРУКТОР MyQueue()");
     }
 
 //***************************************************************************************
     private class Node {
-
-        Node prevNode;
         Object item;
         Node nextNode;
 
-        Node(Node prevNode, Object item, Node nextNode) {
-            this.prevNode = prevNode;
+        Node(Object item, Node nextNode) {
             this.item = item;
-            this.nextNode = nextNode;
-        }
-
-        public Node getPrevNode() {
-            return prevNode;
-        }
-
-        public void setPrevNode(Node prevNode) {
-            this.prevNode = prevNode;
-        }
-
-        public Object getItem() {
-            return item;
-        }
-
-        public void setItem(Object item) {
-            this.item = item;
-        }
-
-        public Node getNextNode() {
-            return nextNode;
-        }
-
-        public void setNextNode(Node nextNode) {
             this.nextNode = nextNode;
         }
 
         @Override
         public String toString() {
             return "Node{ " +
-                    prevNode +
-            ", " +
                     item +
-            ", " +
+                    ", " +
                     nextNode +
-            " }";
+                    " }";
         }
     }
-//***************************************************************************************
-
-//    Node firstNode = new Node(null, null, lastNode);
-//    Node lastNode = new Node(firstNode, null, null);
-
 //************************ Добавляем елемент в конец по значению ************************
     public boolean add(Object value) {
+
+        Node buffer = lastNode;
+        Node node = new Node(value, null);
+        lastNode = node;
+
         if (size == 0) {
-            Node node = new Node(null, value, null);
             firstNode = node;
-            lastNode = node;
             size++;
             System.out.println("Создали ПЕРВУЮ ноду");
         } else {
-            Node node = new Node(lastNode, value, null);
-//            lastNode.nextNode = node;
-            lastNode = node;
+            buffer.nextNode = node;
             size++;
             System.out.println("Создали ПОСЛЕДУЮЩУЮ ноду");
         }
@@ -112,18 +51,12 @@ public class MyQueue {
     }
 //************************ Удаляем елемент по индексу ***********************************
     public boolean remove(int index) {
-        Node nodeToRemove = firstNode;
-        for (int i=0; i<index; i++) {
-            nodeToRemove = nodeToRemove.getNextNode();
-            System.out.println("i = " + i);
-            System.out.println("nodeToRemove = " + nodeToRemove);
+        Node prevNodeToRemove = firstNode;
+        for (int i=0; i<(index-1); i++) {
+            prevNodeToRemove = prevNodeToRemove.nextNode;
         }
-        System.out.println("Удаляем елемент ноды " + nodeToRemove.getItem());
-        nodeToRemove.getNextNode().setPrevNode(nodeToRemove.getPrevNode());
-        nodeToRemove.getPrevNode().setNextNode(nodeToRemove.getNextNode());
-        nodeToRemove = null;
+        prevNodeToRemove.nextNode = prevNodeToRemove.nextNode.nextNode;
         size--;
-//        kolNod--;
         System.out.println("Удалили успешно!!!");
         return true;
     }
@@ -131,8 +64,13 @@ public class MyQueue {
     public boolean clear() {
         boolean rez = false;
 
-//        lastNode = new Node(firstNode, null, null);
-//        firstNode = new Node(null, null, lastNode);
+        for (int i = 0; i < size; i++) {
+            if (i!=(size-1)) {
+                System.out.println("Удаляем " + i + "-й елемент ");
+            }
+
+        }
+
         rez = true;
         size = 0;
 
@@ -144,12 +82,12 @@ public class MyQueue {
     }
 //************************ Возвращаем первый елемент в очереди (FIFO) *******************
     public Object peek() {
-        return (firstNode.getItem());
+        return (firstNode.item);
 //        return firstNode;
     }
 //************************ Воздвращаем первый елемент из очереди и УДАЛЯЕМ ЕГО из коллекции
     public Object poll() {
-        Object vihlop = firstNode.getItem();
+        Object vihlop = firstNode.item;
 
         remove(0);
 
